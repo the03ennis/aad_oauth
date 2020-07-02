0cfc9bd025b4f11b73733de94874ec4a66b09085
Suffield Academy <suffieldacademy@Suffields-iMac.local>
2020-07-02 10:01:20 -0400

    relocated Flutter_Secure_storage code

--- lib/helper/auth_storage.dart
+++ lib/helper/auth_storage.dart
@@ -2,16 +2,47 @@ import 'dart:async';
 import 'package:aad_oauth/model/token.dart';
 import "dart:convert" as Convert;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter/foundation.dart'
+    show kIsWeb; //Will be used to determine if web storage is necessary
 
 class AuthStorage {
   static AuthStorage shared = new AuthStorage();
+  AuthStorage storageInstance;
   FlutterSecureStorage _secureStorage = FlutterSecureStorage();
   String _tokenIdentifier;
 
+  ///Modified this constructor: if the program is running on web, it will return
+  ///an intance of webstorage. Otherwise, it will return an instance of FSStorage,
+  ///which utilizes flutter_secure_storage
   AuthStorage({String tokenIdentifier = "Token"}) {
     _tokenIdentifier = tokenIdentifier;
+    if (kIsWeb) {
+      storageInstance =
+          new WebStorageInstance(tokenIdentifier: tokenIdentifier);
+    } else {
+      storageInstance = new FSStorageInstance(tokenIdentifier: tokenIdentifier);
+    }
   }
 
+  Future<void> saveTokenToCache(Token token) async {
+    storageInstance.saveTokenToCache(token);
+  }
+
+  Future<T> loadTokenToCache<T extends Token>() async {
+    return storageInstance.loadTokenToCache();
+  }
+
+  Token _getTokenFromMap<T extends Token>(Map<String, dynamic> data) =>
+      Token.fromJson(data);
+
+  Future clear() async {
+    storageInstance.clear();
+  }
+}
+
+class WebStorageInstance extends AuthStorage {}
+
+class FSStorageInstance extends AuthStorage {
   Future<void> saveTokenToCache(Token token) async {
     var data = Token.toJsonMap(token);
     var json = Convert.jsonEncode(data);
